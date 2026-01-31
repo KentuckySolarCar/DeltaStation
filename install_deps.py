c16a42c6a032f5323ad2fc230d88aa6357e264d0
Ben Carey <benjamin.james.carey3@gmail.com>
2026-01-31 12:23:00 -0500

    actions: remove redundant builds, fix win64 build
    It comes to no surprise that building with both gcc and clang for the
    same system is really dumb. We have decided to lock in on clang for both
    Linux builds to reduce maintenance overhead. We also switched the
    Windows build to clang so that it would match the compiler for the rest
    of them.

--- install_deps.py
+++ install_deps.py
@@ -4,6 +4,9 @@ import subprocess
 import platform as plat
 
 if plat.uname().system == "Linux":
+    subprocess.run(["add-apt-repository", "universe"])
+    subprocess.run(["apt", "update"])
+
     ubuntu_deps = ["freeglut3-dev", "libwayland-dev", "libxkbcommon-dev", "xorg-dev"]
     subprocess.run(["apt", "install", "-y"] + ubuntu_deps)
 else:
